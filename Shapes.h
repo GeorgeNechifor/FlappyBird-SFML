200f280b9c6c0e2337dbbb45bb42f83b9df56990
GeorgeNechifor <georgenechifor01@gmail.com>
2024-01-02 18:57:13 +0200

    update shape moving

--- Shapes.h
+++
@@ -1,9 +0,0 @@
-
-class Shapes{
-protected:
-    const int shapeModel[3][3] = {
-            {0, 1, 0},
-            {0, 1, 0},
-            {0, 1, 1}
-    };
-};
--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <SFML/Graphics.hpp>
-#include"ScreenTemplate.h"
-#include"DrawShapes.h"
+
 #include<iostream>
 
-const float screen_width = 700;
-const float screen_height = 900;
-const float block_size = 50;
-const float block_final = 500;
+const int screen_width = 900;
+const int screen_height = 700;
 
+int nr = 1;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(screen_width , screen_height) , "TETRIS");
+    sf::RenderWindow window(sf::VideoMode(screen_width , screen_height) , "Flappy Bird");
     window.setFramerateLimit(60);
-    ScreenTemplate temp;
-    DrawShapes shape;
+
 
     while (window.isOpen())
     {
@@ -22,12 +19,10 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            shape.ShapeEvent(event);
         }
 
         window.clear(sf::Color::White);
-        temp.setBlockField(window , block_size , screen_height , block_final);
-        shape.ShapeMove(window);
+
         window.display();
     }
 
